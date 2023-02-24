c277b75aa9d124ae0d21f3ca4adf62e268c72d74
DSMalamov <dsmalamov@gmail.com>
2023-02-24 23:07:48 +0200

    ProblemsSolved

---
+++ Inheritance-Exercise/Person/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
