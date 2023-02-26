686a79f08f3f4725b85665c91ed6f7f1e7125390
DSMalamov <dsmalamov@gmail.com>
2023-02-26 23:20:33 +0200

    ProblemsSolved

---
+++ Inheritance-Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMilliliters = 50;
+        private const decimal CoffeePrice = 3.50M;
+        public Coffee(string name, double caffeine) 
+            : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
