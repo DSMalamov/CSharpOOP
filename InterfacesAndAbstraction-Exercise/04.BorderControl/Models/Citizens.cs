358cf72d5ee15642911fcb1af73de57604b0175e
DSMalamov <dsmalamov@gmail.com>
2023-03-03 23:47:11 +0200

    ProblemsSolved

---
+++ InterfacesAndAbstraction-Exercise/04.BorderControl/Models/Citizens.cs
@@ -0,0 +1,22 @@
+﻿using _04.BorderControl.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.BorderControl.Models;
+
+public class Citizens : IIdentifiable
+{
+    public Citizens(string name, int age, string id)
+    {
+        Name = name;
+        Age = age;
+        Id = id;
+    }
+
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+    public string Id { get; private set; }
+}
---
+++ InterfacesAndAbstraction-Exercise/04.BorderControl/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using _04.BorderControl.Models;
+using _04.BorderControl.Models.Interface;
+
+namespace _04.BorderControl;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<IIdentifiable> collection = new();
+
+        string command;
+
+        while ((command = Console.ReadLine()) != "End")
+        {
+            string[] cmdArg = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            if (cmdArg.Length == 3)
+            {
+                IIdentifiable citizen = new Citizens(cmdArg[0], int.Parse(cmdArg[1]), cmdArg[2]);
+                collection.Add(citizen);
+            }
+            else if (cmdArg.Length == 2)
+            {
+                IIdentifiable robot = new Robot(cmdArg[0], cmdArg[1]);
+                collection.Add(robot);
+            }
+        }
+
+        string controll = Console.ReadLine();
+
+        foreach (var item in collection)
+        {
+            if (item.Id.EndsWith(controll))
+            {
+                Console.WriteLine(item.Id);
+            }
+        }
+    }
+}
